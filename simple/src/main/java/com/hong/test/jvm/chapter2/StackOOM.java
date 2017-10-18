52370b2202312b6be0839927f965d126afcdcd95
caihongwei <caihongwei@mochui.com>
2017-10-18 10:13:44 +0800

    JVM测试代码

--- simple/src/main/java/com/hong/test/jvm/chapter2/StackOOM.java
+++ simple/src/main/java/com/hong/test/jvm/chapter2/StackOOM.java
@@ -2,8 +2,9 @@ package com.hong.test.jvm.chapter2;
 
 /**
  * 如果虚拟机在扩展栈的时候无法申请到足够的内存空间，则抛出OutOfMemoryError异常
- *
  * VM Args: -Xss2m
+ *
+ * 谨慎运行，会导致操作系统死机
  *
  * Created by caihongwei on 17/10/2017 9:13 PM.
  */
