d6e8811ff607cfe0a6a7167e3ae5e01f2f11e258
caihongwei <caihongwei@mochui.com>
2017-12-29 14:11:44 +0800

    protobuf

---
+++ simple/src/main/java/com/hong/test/protobuf/Main.java
@@ -0,0 +1,10 @@
+package com.hong.test.protobuf;
+
+/**
+ * Created by caihongwei on 11/12/2017 10:53 AM.
+ */
+public class Main {
+    public static void main(String[] args) {
+        AddressBookOuterClass.AddressBook book = AddressBookOuterClass.AddressBook.getDefaultInstance();
+    }
+}
